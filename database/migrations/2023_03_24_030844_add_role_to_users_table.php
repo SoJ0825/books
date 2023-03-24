685458b5bd4a9c723598af783a69039ce7de1d17
SOJ <takayashi0825@gmail.com>
2023-03-24 11:14:56 +0800

    feat: add role to users table

---
+++ database/migrations/2023_03_24_030844_add_role_to_users_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('role')->default(User::ROLE_NORMAL)->after('id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
